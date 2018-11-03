5151cd299c60a012e6f98e8192c19b2788c37122
Yolanda H <huangyalan@yolanda-h-macbook-air.local>
2018-11-04 03:54:36 +0800

    Initial Commit

---
+++ trya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trya
+//
+//  Created by Yolanda H. on 2018/11/4.
+//  Copyright © 2018 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
